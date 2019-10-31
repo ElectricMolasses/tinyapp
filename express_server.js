2f875bf4564ee3a54e679e1656f4a0f724ff26c8
Robert Beckett <robert.beckett@onion.red>
2019-10-31 00:58:44 +0000

    Implemented delete url with method overriding.

--- express_server.js
+++ express_server.js
@@ -122,7 +122,7 @@ app.post("/urls", (req, res) => {
   }
 });
 
-app.delete("/urls/:shortURL/delete", (req, res) => {
+app.delete("/urls/:shortURL", (req, res) => {
 
   if (userIDExists(req.session, users) &&
         urlDatabase[req.params.shortURL] &&
